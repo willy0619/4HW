c5a3d5e96510b6e68ebc5d2010b601fe5dc8ff0e
willy0619 <74764061+willy0619@users.noreply.github.com>
2021-06-10 14:52:45 +0800

    Update DropdownListShow.aspx.designer.cs

--- 4HW/DropdownListShow.aspx.designer.cs
+++ 4HW/DropdownListShow.aspx.designer.cs
@@ -7,10 +7,12 @@
 // </自動產生的>
 //------------------------------------------------------------------------------
 
-namespace _4HW {
+namespace _4HW
+{
 
 
-    public partial class DropdownListShow {
+    public partial class DropdownListShow
+    {
 
         /// <summary>
         /// form1 控制項。
@@ -20,5 +22,23 @@ namespace _4HW {
         /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+
+        /// <summary>
+        /// ddl_Area 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_Area;
+
+        /// <summary>
+        /// ddl_Place 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。 
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddl_Place;
     }
 }
